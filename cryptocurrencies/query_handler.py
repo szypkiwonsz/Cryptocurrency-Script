61f32f9202d1134302193a43c7bb09ef32e64900
szypkiwonsz <kacpersawicki321@gmail.com>
2021-04-01 20:10:57 +0200

    Add QueryHandler class storing methods for basics database queries

---
+++ cryptocurrencies/query_handler.py
@@ -0,0 +1,46 @@
+from datetime import timedelta
+
+from database_handler import Currency
+
+
+class QueryHandler:
+    """Class storing methods for basics database queries."""
+
+    @staticmethod
+    def get_all_currencies():
+        """
+        Gets all currencies data from the database.
+        :return: <peewee.ModelSelect> -> class with currencies data
+        """
+        return Currency.select()
+
+    def get_all_currencies_by_name(self, currency_name):
+        """
+        Gets all currencies data by name from the database.
+        :param currency_name: <str> -> currency name
+        :return: <peewee.ModelSelect> -> class with currencies data by name
+        """
+        return self.get_all_currencies().where(Currency.name == currency_name)
+
+    def get_all_currencies_by_name_between_dates(self, start_date, end_date, currency_name):
+        """
+        Gets all currencies data by name between dates from the database.
+        :param start_date: <datetime.datetime> -> start date of the searched data
+        :param end_date: <datetime.datetime> -> end date of the searched data
+        :param currency_name: <str> -> currency name
+        :return: <peewee.ModelSelect> -> class with currencies data by name between provided dates
+        """
+        return self.get_all_currencies_by_name(currency_name).where(Currency.time_close.between(
+            start_date, end_date + timedelta(days=1)))
+
+    def get_currencies_column_by_name_between_dates(self, start_date, end_date, currency_name, column_name):
+        """
+        Gets list with values selected by database column, currency name and between provided dates.
+        :param start_date: <datetime.datetime> -> start date of the searched data
+        :param end_date: <datetime.datetime> -> end date of the searched data
+        :param currency_name: <str> -> currency name
+        :param column_name: <str> -> database column name
+        :return: <list> -> list of values from selected column
+        """
+        currencies = self.get_all_currencies_by_name_between_dates(start_date, end_date, currency_name).dicts()
+        return [currency[column_name] for currency in currencies]
