968f6e343c0259dc229de3a63705579773ad042c
szypkiwonsz <kacpersawicki321@gmail.com>
2021-04-03 15:54:10 +0200

    Add tests for PriceHandler class
    Added tests for the functions of the 'PriceHandler' class.
    Data needed to run the tests has been added.
    Tests marker added.

--- tests/test_query_handler.py
+++ tests/test_query_handler.py
@@ -22,3 +22,17 @@ class TestDateHandler:
     def test_get_all_currencies_by_name_between_dates(self, date_handler, database_with_data):
         assert date_handler.get_all_currencies_by_name_between_dates(
             datetime(2011, 1, 1), datetime(2011, 1, 3), 'btc-bitcoin').dicts()[-1]['close'] == 0.7
+
+
+@pytest.mark.price_handler
+class TestPriceHandler:
+
+    def test_get_currencies_prices_by_day(self, price_handler, query_handler, database_with_data):
+        currencies = query_handler.get_all_currencies_by_name('btc-bitcoin').dicts()
+        price_list = price_handler.get_currencies_prices_by_day(currencies)
+        assert price_list.to_dict('records')[0]['price'] == 0.3
+
+    def test_get_currency_price_by_day_between_dates(self, price_handler, database_with_data):
+        price_list = price_handler.get_currency_price_by_day_between_dates(
+            datetime(2011, 1, 1), datetime(2011, 1, 2), 'usdt-tether')
+        assert price_list.to_dict('records')[0]['price'] == 0.4
