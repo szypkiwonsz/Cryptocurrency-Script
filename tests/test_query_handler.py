b44bc9ead6215586dcf41a5c99000ebd8bdf9f8c
szypkiwonsz <kacpersawicki321@gmail.com>
2021-04-03 15:28:46 +0200

    Add tests for DateHandler class
    Added tests for the functions of the 'DateHandler' class.
    Data needed to run the tests has been added.
    Tests marker added.

--- tests/test_query_handler.py
+++ tests/test_query_handler.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 
 
@@ -12,3 +14,11 @@ class TestQueryHandler:
 
     def test_get_currency_column_by_name(self, query_handler, database_with_data):
         assert query_handler.get_currency_column_by_name('btc-bitcoin', 'close')[0] == 0.3
+
+
+@pytest.mark.date_handler
+class TestDateHandler:
+
+    def test_get_all_currencies_by_name_between_dates(self, date_handler, database_with_data):
+        assert date_handler.get_all_currencies_by_name_between_dates(
+            datetime(2011, 1, 1), datetime(2011, 1, 3), 'btc-bitcoin').dicts()[-1]['close'] == 0.3
