c189cc8d2ad7ccb48f8bc8a1e7167e5a11be4600
szypkiwonsz <kacpersawicki321@gmail.com>
2021-04-05 14:06:07 +0200

    Add tests for command in script.py file
    Added tests for the 'consecutive_increase' command.

--- tests/test_script.py
+++ tests/test_script.py
@@ -6,7 +6,7 @@ from click.testing import CliRunner
 from dateutil.relativedelta import relativedelta
 from httmock import HTTMock, all_requests
 
-from script import average_price_by_month
+from script import average_price_by_month, consecutive_increase
 from tests.conftest import api_content
 
 
@@ -50,4 +50,42 @@ class TestAveragePriceByMonth:
 
     def test_average_price_by_month_wrong_arguments(self):
         response = runner.invoke(average_price_by_month, ['--start_date=2012-01-01', f'--end_date=2011-12-01'])
+        assert response.exit_code == 2
+
+
+@pytest.mark.script
+class TestConsecutiveIncrease:
+
+    def test_consecutive_increase(self, database):
+        with patch('database_handler.DatabaseHandler.db') as mock:
+            mock.return_value = database
+            with HTTMock(api_mock):
+                response = runner.invoke(consecutive_increase, ['--start_date=2012-01-01', '--end_date=2012-02-03'])
+                assert response.exit_code == 0
+                assert '$17.66' in response.output
+
+    def test_consecutive_increase_diff_coin(self, database):
+        with patch('database_handler.DatabaseHandler.db') as mock:
+            mock.return_value = database
+            with HTTMock(api_mock):
+                response = runner.invoke(consecutive_increase, [
+                    '--start_date=2012-01-01', '--end_date=2012-02-03', '--coin=usdt-tether'
+                ])
+                assert response.exit_code == 0
+                print(response.output)
+                assert '$17.66' in response.output
+
+    def test_consecutive_increase_too_late_end_date(self):
+        next_month = datetime.now() + relativedelta(months=1)
+        response = runner.invoke(consecutive_increase, [
+            '--start_date=2012-01-01', f'--end_date={str(next_month)[:10]}'
+        ])
+        assert response.exit_code == 2
+
+    def test_consecutive_increase_end_date_before_start_date(self):
+        response = runner.invoke(consecutive_increase, ['--start_date=2012-01-01', f'--end_date=2011-12-01'])
+        assert response.exit_code == 2
+
+    def test_consecutive_increase_wrong_arguments(self):
+        response = runner.invoke(consecutive_increase, ['--start_date=2012-01', f'--end_date=2011-12'])
         assert response.exit_code == 2
